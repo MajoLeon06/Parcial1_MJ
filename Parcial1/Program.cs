ad60c83280c74c47456025535055bc80dcb8bfcd
MajoLeon06 <majoleonpaz1321@gmail.com>
2026-02-25 10:43:16 -0600

    Agregar archivos de proyecto.

---
+++ Parcial1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese el nombre del estudiante:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingresar nota:");
+double nota = double.Parse(Console.ReadLine());
+if (nota>=70)
+{
+    Console.WriteLine($"{nombre}, Aprobado");
+}
+else
+{
+    Console.WriteLine($"{nombre}, Reprobado");
+}
